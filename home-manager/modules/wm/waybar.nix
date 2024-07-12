fbe56b699d0678a2429501ac30c7141a11e918d3
Перов Николай <80873179+Resuwu@users.noreply.github.com>
2024-07-12 10:29:47 +0300

    Waybar test

--- home-manager/modules/wm/waybar.nix
+++ home-manager/modules/wm/waybar.nix
@@ -1,4 +1,35 @@
 { programs.waybar = {
         enable = true;
+        settings = {
+            mainBar = {
+                layer = "top";
+                position = "top";
+                margin = "9 13 -10 18";
+
+                modules-left = ["hyprland/workspaces" "hyprland/language" "keyboard-state" "hyprland/submap"];
+                modules-center = ["clock" "custom/weather"];
+                modules-right = ["pulseaudio" "custom/mem" "cpu" "backlight" "battery" "tray"];
+
+                "hyprland/workspaces" = {
+                    disable-scroll = true;
+                };
+
+                "hyprland/language" = {
+                    format-en = "US";
+                    format-ru = "RU";
+	                min-length = 5;
+	                tooltip = false;
+                };
+
+                "keyboard-state" = {
+                    capslock = true;
+                    format = "{icon} ";
+                    format-icons = {
+                        locked = " ";
+                        unlocked = "";
+                    };
+                };
+            };
+        };
     };
 }
