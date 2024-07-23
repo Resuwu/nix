b255a78e47866dbd7efe8634378af78fc9b1c283
Перов Николай <80873179+Resuwu@users.noreply.github.com>
2024-07-23 11:58:56 +0300

    Fix

--- nixos/modules/sddm.nix
+++ nixos/modules/sddm.nix
@@ -5,12 +5,4 @@
         theme = "catppuccin-mocha";
         package = pkgs.kdePackages.sddm;
     };
-
-    environment.systemPackages = with pkgs; [
-        (catppuccin-sddm.override {
-            flavor = "mocha";
-            font = "JetBrains Mono Semibold";
-            fontSize = "13";
-        })
-    ];
 }
--- nixos/packages.nix
+++ nixos/packages.nix
@@ -1,56 +1,61 @@
 { pkgs, unstable, ... }: {
 
     environment.systemPackages =
-        (with unstable; [
-            firefox
-            anilibria-winmaclinux
-            spotify
-            telegram-desktop
-            webcord
-            obsidian
-            jetbrains.idea-community
-            path-of-building
-        ])
+    (with unstable; [
+        firefox
+        anilibria-winmaclinux
+        spotify
+        telegram-desktop
+        webcord
+        obsidian
+        jetbrains.idea-community
+        path-of-building
+    ])
 
-        ++
+    ++
 
-        (with pkgs; [
-            alacritty
-            imv
+    (with pkgs; [
+        alacritty
+        imv
 
-            file
-            tree
-            zip
-            unzip
-            git
-            cava
-            lux
-            fastfetch
-            openssl
-            htop
+        file
+        tree
+        zip
+        unzip
+        git
+        cava
+        lux
+        fastfetch
+        openssl
+        htop
 
-            bluez
-            bluez-tools
-            bluez-alsa
+        bluez
+        bluez-tools
+        bluez-alsa
 
-            hyprland
-            xdg-desktop-portal-hyprland
-            hyprlock
-            hypridle
+        hyprland
+        xdg-desktop-portal-hyprland
+        hyprlock
+        hypridle
+        xwayland
+        fuzzel
+        swww
+        mako
+        waybar
+        networkmanagerapplet
 
-            xwayland
-            fuzzel
-            swww
-            mako
-            waybar
-            networkmanagerapplet
-            playerctl
+        playerctl
+        pipewire
+        pulseaudio
+        pamixer
 
-            pipewire
-            pulseaudio
-            pamixer
-
-            home-manager
-	        wireguard-tools
-        ]);
+        home-manager
+	    wireguard-tools
+        
+        (catppuccin-sddm.override {
+            flavor = "mocha";
+            font = "JetBrains Mono Semibold";
+            fontSize = "13";
+        })
+    ]);
 }
