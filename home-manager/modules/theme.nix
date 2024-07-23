0c83fd110b6039932b2325809aec816699c5d0fe
Перов Николай <80873179+Resuwu@users.noreply.github.com>
2024-07-23 13:29:14 +0300

    Fix theme

--- home-manager/modules/theme.nix
+++ home-manager/modules/theme.nix
@@ -1,17 +1,17 @@
 { pkgs, ... }: {
-    home = {
-		gtk = {
-			enable = true;
-			theme = {
-				package = (pkgs.catppuccin-gtk.override {
-      				accents = [ "green" ];
-      				size = "standard";
-      				variant = "mocha";
-    			});
-				name = "catppuccin-mocha-green-standard";
-			};
+	gtk = {
+		enable = true;
+		theme = {
+			package = (pkgs.catppuccin-gtk.override {
+      			accents = [ "green" ];
+      			size = "standard";
+      			variant = "mocha";
+    		});
+			name = "catppuccin-mocha-green-standard";
 		};
-		
+	};
+	
+    home = {
 		pointerCursor = {
 		    package = pkgs.catppuccin-cursors.mochaGreen;
 		    name = "Catppuccin-Mocha-Green";
