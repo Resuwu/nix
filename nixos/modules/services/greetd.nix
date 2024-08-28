d58ed5ca7eb20b2fb6308224e54f04f87433d5b6
Перов Николай <80873179+Resuwu@users.noreply.github.com>
2024-08-28 12:09:41 +0300

    Greetd customisation

--- nixos/modules/services/greetd.nix
+++ nixos/modules/services/greetd.nix
@@ -1,11 +1,16 @@
-{ pkgs, ... }: {
+{ pkgs, ... }: 
+let
+    tuigreet = "${pkgs.greetd.tuigreet}/bin/tuigreet";
+    theme = "border=magenta;text=cyan;prompt=green;time=red;action=blue;button=yellow;container=black;input=red";
+in {
     services.greetd = {
         enable = true;
         settings = {
             default_session = {
-                command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd hyprland";
+                command = "${tuigreet} --time --remember --asterisks --theme ${theme} --cmd hyprland";
                 user = "greeter";
             };
         };
+        vt = 2;
     };
 }
