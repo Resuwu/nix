2d417e5cf6096e24dbde7d29c0459544c6772699
Перов Николай <80873179+Resuwu@users.noreply.github.com>
2024-08-30 15:58:19 +0300

    ff modules

--- home-manager/modules/programs/fastfetch.nix
+++ home-manager/modules/programs/fastfetch.nix
@@ -4,8 +4,49 @@
         settings = {
             logo = {
                 source = "~/nix/home-manager/imgs/ffimage";
-                color = "cyan";
+                color = {
+                    "1" = "cyan";
+                };
             };
+            modules = [
+                "title"
+                "separator"
+                "os"
+                "host"
+                {
+                    "type" = "kernel";
+                    "format" = "{release}";
+                }
+                "uptime"
+                "packages"
+                "shell"
+                {
+                    "type" = "display";
+                    "compactType" = "original";
+                    "key" = "Resolution";
+                }
+                "de"
+                "wm"
+                "wmtheme"
+                "theme"
+                "icons"
+                "terminal"
+                {
+                    "type" = "terminalfont";
+                    "format" = "{/name}{-}{/}{name}{?size} {size}{?}";
+                }
+                "cpu"
+                {
+                    "type" = "gpu";
+                    "key" = "GPU";
+                }
+                {
+                    "type" = "memory";
+                    "format" = "{} / {}";
+                }
+                "break"
+                "colors"
+            ];
         };
     };
 }
