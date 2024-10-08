3760e30577f05ad3b5011c4b44cd797801326798
Перов Николай <80873179+Resuwu@users.noreply.github.com>
2024-10-08 10:14:32 +0300

    disabled silent boot

--- nixos/modules/system/boot.nix
+++ nixos/modules/system/boot.nix
@@ -5,19 +5,19 @@
             efi.canTouchEfiVariables = true;
         };
 
-        initrd.verbose = false;
-        consoleLogLevel = 0;
-        kernelParams = [ "quiet" "udev.log_level=3" ];
+#       initrd.verbose = false;
+#       consoleLogLevel = 0;
+#       kernelParams = [ "quiet" "udev.log_level=3" ];
 
-        plymouth = {
-            enable = true;
-            theme = "angular_alt";
-            themePackages = with pkgs; [
-                (adi1090x-plymouth-themes.override {
-                    selected_themes = [ "angular_alt" ];
-                })
-            ];
-        };
+#       plymouth = {
+#           enable = true;
+#           theme = "angular_alt";
+#           themePackages = with pkgs; [
+#               (adi1090x-plymouth-themes.override {
+#                   selected_themes = [ "angular_alt" ];
+#               })
+#           ];
+#       };
 
         blacklistedKernelModules = [ "uvcvideo" ];
     };
