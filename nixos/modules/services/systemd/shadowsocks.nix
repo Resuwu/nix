71e94896f6a1ef7e522e9f798a25f4982b1d1549
Перов Николай <80873179+Resuwu@users.noreply.github.com>
2024-10-18 10:42:03 +0300

    shadowsocks service fix

--- nixos/modules/services/systemd/shadowsocks.nix
+++ nixos/modules/services/systemd/shadowsocks.nix
@@ -2,14 +2,14 @@
   configPath = "$HOME/.config/shadowsocks/config.json";
 in {
   systemd.user.services.shadowsocks-proxy = {
-      description = "Local Shadowsocks proxy";
-      after = "network.target";
+    description = "Local Shadowsocks proxy";
+    after = ["network.target"];
 
-      serviceConfig = {
-        ExecStart = "${pkgs.shadowsocks-rust}/bin/sslocal -c ${configPath}";
-        ExecStop = "${pkgs.toybox}/bin/killall sslocal";
-      };
+    serviceConfig = {
+      ExecStart = "${pkgs.shadowsocks-rust}/bin/sslocal -c ${configPath}";
+      ExecStop = "${pkgs.toybox}/bin/killall sslocal";
+    };
 
-      wantedBy = ["default.target"];
+    wantedBy = ["default.target"];
   };
 }
